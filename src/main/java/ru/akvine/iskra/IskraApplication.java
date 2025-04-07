a751aa204be0c3d3e58548619e734e62f0f743a6
akvine <redgooseman55@gmail.com>
2025-04-08 00:47:19 +0300

    init project

---
+++ src/main/java/ru/akvine/iskra/IskraApplication.java
@@ -0,0 +1,13 @@
+package ru.akvine.iskra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IskraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IskraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akvine/iskra/IskraApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.iskra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IskraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
